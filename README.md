22098c93ae3360718019342bcc724e58b7e3670c
leaf4monkey <joc98@163.com>
2017-05-12 10:55:14 +0800

    1st commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# OAuth-Helpers
+
+Help you create oauth packages rapidly.
+
+## See
+
+[oauth-helpers-server](/leaf4monkey/oauth-helpers-server) in server.
+
+**or**
+
+[oauth-helpers-client](/leaf4monkey/oauth-helpers-client) in client.
---
+++ package.js
@@ -0,0 +1,30 @@
+var packageName = 'leaf4monkey:oauth-helpers-common';
+var mainModule = './lib/index.js';
+var mainModuleEnv = mainModuleEnv;
+Package.describe({
+  name: packageName,
+  version: '0.0.1',
+  summary: 'Help you create oauth packages rapidly.',
+  git: 'git@github.com:leaf4monkey/oauth-helpers-common.git',
+  documentation: 'README.md'
+});
+
+var commonDependencies = function (api) {
+  api.use('ecmascript');
+  api.use('meteor', mainModuleEnv);
+  api.use('service-configuration', mainModuleEnv);
+};
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.1');
+  commonDependencies(api);
+
+  api.mainModule(mainModule, mainModuleEnv);
+});
+
+//Package.onTest(function(api) {
+//  commonDependencies(api);
+//  api.use('tinytest');
+//  api.addFiles(mainModule, mainModuleEnv);
+//  api.mainModule('oauth-helpers-tests.js', mainModuleEnv);
+//});
