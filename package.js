e5dfae55b79967134a35b9db7cfeda8124e8c6f3
leaf4monkey <joc98@163.com>
2017-05-12 11:32:26 +0800

    upgrade to 0.0.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var mainModule = './lib/index.js';
 var mainModuleEnv = mainModuleEnv;
 Package.describe({
   name: packageName,
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Help you create oauth packages rapidly.',
   git: 'git@github.com:leaf4monkey/oauth-helpers-common.git',
   documentation: 'README.md'
