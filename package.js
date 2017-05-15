dfd862799d034aad6f01742d4521e7d3cf630e0d
leaf4monkey <joc98@163.com>
2017-05-15 09:57:07 +0800

    upgrade to 0.0.8

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 var packageName = 'leaf4monkey:oauth-helpers-common';
 var mainModule = './lib/index.js';
 var mainModuleEnv = mainModuleEnv;
-var version = '0.0.6';
+var version = '0.0.8';
 
 Package.describe({
   name: packageName,
